2842fae4b8a3ad71ff13632a6bd5569d8974fef4
Chad Schadewald <ces83@students.uwf.edu>
2021-02-17 16:09:44 -0600

    Working through tutorial

--- dags/my_sample_dag.py
+++ dags/my_sample_dag.py
@@ -1,4 +1,22 @@
 from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+
+args = {
+    'owner': 'schadewald',
+    'start_date': days_ago(1)
+}
+
+dag = DAG(dag_id='my_sample_dag', default_args=args, schedule_interval=None)
 
 
-dag = DAG()
+def run_this_func():
+    print('Hello World!')
+
+
+with dag:
+    run_this_task = PythonOperator(
+        task_id='run_this',
+        python_callable=run_this_func,
+        provide_context=True
+    )
